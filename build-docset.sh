904832b46d0f41e25b41b24cd65b626aa261c9e3
Matthieu Caneill <matt@brokenwa.re>
2022-09-26 22:41:44 +0200

    Add script to build Snowflake docset

---
+++ build-docset.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+DOCS_URL="https://docs.snowflake.com/en"
+VENV=".venv"
+OUT="out"
+
+# Create virtual environment if it doesn't exist
+if [ ! -d "${VENV}" ]; then
+    python3 -m venv "${VENV}"
+    python3 -m pip install -r requirements.txt
+fi
+
+# Activate virtual environment
+source "${VENV}/bin/activate"
+
+# Start from a clean state
+rm -rf "${OUT}"
+mkdir "${OUT}"
+
+# Download Snowflake documentation
+cd "${OUT}"
+wget -nH --cut-dirs=1 -r --no-parent "${DOCS_URL}"
+wget "${DOCS_URL}/objects.inv"
+wget "${DOCS_URL}/_static/favicon-32x32.png"
+cd ..
+
+# Build docset
+doc2dash -n Snowflake -i "${OUT}/favicon-32x32.png" "${OUT}"
+
+# Compress docset
+tar -czf Snowflake.tgz Snowflake.docset
