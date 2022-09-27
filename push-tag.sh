4e41dc80be3286feecf261567f634726b7f972e3
Matthieu Caneill <matt@brokenwa.re>
2022-09-27 20:38:33 +0200

    Use YYYY.MM.DD in git tags, make them semver

--- push-tag.sh
+++ push-tag.sh
@@ -3,8 +3,11 @@
 if [[ ! -z "${GITHUB_ACTOR}" ]];then
     git config user.name "${GITHUB_ACTOR}"
     git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+    SUFFIX=""
+else
+    SUFFIX="-manual"
 fi
 
-TAG=$(date +v%Y%m%d-%H%M%S)
+TAG=$(date +v%Y.%m.%d)"${SUFFIX}"
 git tag "${TAG}"
 git push origin "${TAG}"
